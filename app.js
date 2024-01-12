5e9a7a136f182d9fda872422ce0893365a83ecc3
André Ramaldes <67028873+Andre07-creator@users.noreply.github.com>
2024-01-12 10:19:06 -0300

    add and modified adicionado as rotas path, instalado a engine ejs, criado novas pasta: views, layouts, pages, public. acrescentado o bulma para o css, criado o index principal do tipo ejs

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 const checkListRouter = require("./src/routes/checklist");
+const rootRouter = require("./src/routes/index");
 require("./config/database");
-const app = express();
 
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", rootRouter);
 app.use("/checklists", checkListRouter);
+app.set("views", path.join(__dirname, "src/views"));
+
 
 app.listen(3000, () => {
   console.log("Servidor iniciado!");
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,6 +7,7 @@ router.get("/", async (req, res) => {
     let checklists = await Checklist.find({});
     res.status(200).json(checklists);
   } catch (error) {
+    console.log("erro no get")
     res.status(500).json(error);
   }
 });
@@ -28,6 +29,31 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(checklist);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let { name } = { name: "ola mundo3" };
+  try {
+    let checklist = await Checklist.findByIdAndUpdate(
+      req.params.id,
+      { name },
+      { new: true }
+    );
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(433).json(error);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log(req.params.id)
+    const checklist = await Checklist.findByIdAndDelete(req.params.id);
+    
+    res.status(200).json(checklist);
+  } catch (error) {
+    res.status(433).json(error)
   }
 });
 
